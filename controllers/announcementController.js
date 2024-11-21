d5fee2ba5f329878a2657cf6a035e8a064203921
Lucas Costa <lucas.costa@simova.com.br>
2024-11-21 12:25:50 -0300

    feat: cria camada de 'controllers' da aplicação

---
+++ controllers/announcementController.js
@@ -0,0 +1,7 @@
+const { Announcement } = require('../models/Announcement');
+
+const announcementController = {
+
+}
+
+module.exports = announcementController;
---
+++ controllers/sponsorController.js
@@ -0,0 +1,7 @@
+const { Sponsor } = require('../models/Sponsor');
+
+const sponsorController = {
+
+}
+
+module.exports = sponsorController;
