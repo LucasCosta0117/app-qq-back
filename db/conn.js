0c285f54a376aee8c560f72e8e857aec54a0f2c1
Lucas Costa <lucas.costa@simova.com.br>
2024-11-23 19:30:21 -0300

    refactor: altera o código de conexão para explicitar o banco de dados no MongoDB

--- db/conn.js
+++ db/conn.js
@@ -4,9 +4,10 @@ async function main() {
     try {
         const user = process.env.DB_USER;
         const pass = process.env.DB_PASS;
+        const dbName = process.env.DB_NAME;
 
         await mongoose.connect(
-            `mongodb+srv://${user}:${pass}@cluster0.qrmhi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+            `mongodb+srv://${user}:${pass}@cluster0.qrmhi.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluester0`
         );
     } catch (error) {
         console.log(`Error connecting to database: ${error}`);
