7a86adecd77ad761ecf6f4b26e2f3479ee651ee3
Lucas Costa <lucas.costa@simova.com.br>
2024-11-23 19:35:47 -0300

    feat: cria primeira rota de usuários para cadastrar um novo usuário

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 
 //Rotas de Usuário
+router.route('/user').post((req, res) => userController.create(req, res)) //Cadastrar um usuário
+
 
 module.exports = router;
