7d8f22bb72baf9b9606ee52bf13bb6af02c52354
Lucas Costa <lucas.costa@simova.com.br>
2024-11-26 00:10:58 -0300

    feat: implementa as primeiras rotas da entidade Usuário

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,11 @@ const userController = {
                 email: req.body.email,
                 password: req.body.password
             }
-    
+            
+            /**
+             * @todo validar se o e-mail já existe no banco e está ativo
+             * @todo criptografar a senha do usuário antes de mandar para o db
+             */
             const response = await User.create(newUser);
 
             res
@@ -19,7 +23,7 @@ const userController = {
             .json({
                 response: response,
                 msg: "Usuário criado com sucesso!"
-            })
+            });
         } catch (error) {
             console.log(`Error: ${error}`);
 
@@ -27,13 +31,95 @@ const userController = {
             .status(400)
             .json({
                 response: error,
-                msg: "Falha ao tentar criar o Usuário"
-            })
+                msg: "Falha ao criar o Usuário"
+            });
         }
+    },
+    getAll: async (req, res) => {
+        try {
+            const users = await User.find();
 
+            if (!users) {
+                res
+                .status(404)
+                .json({ 
+                    msg: 'Não existem usuários ativos no momento' 
+                });
 
+                return;
+            }
+            
+            res
+            .status(200)
+            .json(users);
+        } catch (error) {
+            console.log(`Error: ${error}`);
+
+            res
+            .status(400)
+            .json({
+                response: error,
+                msg: "Falha ao obter a lista de Usuários"
+            });
+        }
+    },
+    getAllActive: async (req, res) => {
+        try {
+            const users = await User.find({ active: true });
+
+            if (!users) {
+                res
+                .status(404)
+                .json({
+                    msg: 'Não existem usuários ativos no momento'
+                });
+                
+                return;
+            }
+
+            res
+            .status(200)
+            .json(users);
+        } catch (error) {
+            console.log(`Error: ${error}`);
+
+            res
+            .status(400)
+            .json({
+                response: error,
+                msg: "Falha ao obter a lista de Usuários"
+            });
+        }
+    },
+    getByEmail: async (req, res) => {
+        try {
+            const email = req.params.email;
+            const user = await User.findOne({ email: email });
+
+            if (!user) {
+                res
+                .status(404)
+                .json({
+                    msg: 'Usuário não encontrado!'
+                });
+                
+                return;
+            }
+            
+            res
+            .status(200)
+            .json(user);
+        } catch (error) {
+            console.log(`Error: ${error}`);
+
+            res
+            .status(400)
+            .json({
+                response: error,
+                msg: "Falha ao obter o Usuário com o e-mail fornecido!"
+            });
+        }
     }
-
 }
 
 module.exports = userController;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
 
-//Rotas de Usuário
-router.route('/user').post((req, res) => userController.create(req, res)) //Cadastrar um usuário
+// Rotas de Usuário
+router 
+    .route('/user/create') //Cadastrar um usuário
+    .post((req, res) => userController.create(req, res));
 
+router 
+    .route('/user/get/all') //Listar todos usuários
+    .get((req, res) => userController.getAll(req, res)) 
+
+router
+    .route('/user/get/active') //Listar todos usuários ativos
+    .get((req, res) => userController.getAllActive(req, res))
+
+router
+    .route('/user/get/:email') //Buscar um usuário por email
+    .get((req, res) => userController.getByEmail(req, res))
 
 module.exports = router;
